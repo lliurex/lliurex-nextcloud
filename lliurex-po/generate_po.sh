9fa086be0051a0982bb89f94061e057a88a408fa
Juan Ramon Pelegrina <juapesai@homtail.com>
2021-04-16 14:30:42 +0200

    Added option to remove nextcloud in setup gui

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ../install-files/usr/sbin/lliurex-nextcloud-setup /tmp/lliurex-nextcloud.py
+cp ../lliurex-nextcloud-setup/usr/sbin/lliurex-nextcloud-setup /tmp/lliurex-nextcloud.py
 
-xgettext ../install-files/usr/share/lliurex-nextcloud/rsrc/lliurex-nextcloud-setup.ui /tmp/lliurex-nextcloud.py -o lliurex-nextcloud/lliurex-nextcloud.pot
+xgettext ../lliurex-nextcloud-setup/usr/share/lliurex-nextcloud/rsrc/lliurex-nextcloud-setup.ui /tmp/lliurex-nextcloud.py -o lliurex-nextcloud/lliurex-nextcloud.pot
 
 
